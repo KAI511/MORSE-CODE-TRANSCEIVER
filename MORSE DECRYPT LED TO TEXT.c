92916c7d37c86e3f791e26f44038b5c6ddc1b8ff
SN.KAILASHSUNDARAM <68651615+KAI511@users.noreply.github.com>
2021-08-17 12:56:52 +0530

    Add files via upload

---
+++ MORSE DECRYPT LED TO TEXT.c
@@ -0,0 +1,53 @@
+
+const char MorseTree[] = {'\0','E', 'T', 'I', 'A', 'N', 'M', 'S',
+                          'U', 'R', 'W', 'D', 'K', 'G', 'O', 'H',
+                          'V', 'F', 'U', 'L', 'A', 'P', 'J', 'B',
+                          'X', 'C', 'Y', 'Z', 'Q', '\0','\0','5',
+                          '4', '\0','3', '\0','\0','\0','2', '\0',
+                          '\0','+', '\0','\0','\0','\0','1', '6',
+                          '=', '/', '\0','\0','\0','(', '\0','7',
+                          '\0','\0','\0','8', '\0','9', '0', '\0',
+                          '\0','\0','\0','\0','\0','\0','\0','\0',
+                          '\0','\0','\0','?', '_', '\0','\0','\0',
+                          '\0','"', '\0','\0','.', '\0','\0','\0',
+                          '\0','@', '\0','\0','\0','\0','\0','\0',
+                          '-', '\0','\0','\0','\0','\0','\0','\0',
+                          '\0',';', '!', '\0',')', '\0','\0','\0',
+                          '\0','\0',',', '\0','\0','\0','\0',':',
+                          '\0','\0','\0','\0','\0','\0','\0'
+                         };
+int val = 0; 
+int ctrHigh = 0;
+int ctrLow = 0;
+int codePtr = 0;
+int dotLen = 100;
+void setup()
+{
+  Serial.begin(9600);
+  Serial.print(codePtr);
+}
+void loop()
+{
+  val = analogRead(4);
+  if (val >= 10)
+  {
+    ctrHigh++;
+    ctrLow = 0;
+  } else {
+    ctrLow++;
+    if ((ctrHigh >= dotLen) && (ctrHigh < dotLen*2)) {
+      Serial.print(".");
+      codePtr = (2*codePtr) + 1;
+    } else if (ctrHigh >= dotLen * 2) {
+      Serial.print("-");
+      codePtr = (2*codePtr) + 2;
+    } else {
+      if(ctrLow == dotLen*2){
+        Serial.print(MorseTree[codePtr]);
+        codePtr = 0;
+      }
+    }
+    ctrHigh = 0;
+
+  }
+}
